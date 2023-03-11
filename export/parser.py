db6d11999c5d96bf4a144f04056e4242d879eec9
Mario Valiente <mvc1009@gmail.com>
2023-03-11 21:22:07 +0100

    starting export

---
+++ export/parser.py
@@ -0,0 +1,17 @@
+
+def parse_to_cherry():
+'''
+# Cherry Tree output format
+
+Scope
+  │
+  ├─► Host
+  │   │
+  │   └─► SubDomain
+  │
+  ├─► Domain
+  │
+  │
+  └─► Vulnerabilities
+'''
+	print()
